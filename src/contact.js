bcd7bd4ae23b395593d59f211b5f13b46a3a4808
linustalacko <83816641+linustalacko@users.noreply.github.com>
2022-06-23 19:40:18 +1000

    finally done; '

--- src/contact.js
+++ src/contact.js
@@ -18,12 +18,17 @@ function createContactPage() {
     }
     create_title();
 
-    function myMap() {
-        const uluru = { lat: -25.344, lng = 131.031 };
-        const map = new google.maps.Map(docu)
+    
+    function mapping() {
+        let map = document.createElement('div')
+        map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14951977.760307666!2d-19.277083357112335!3d-23.804897908218756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x25a3b10c91d2619%3A0xe4710f5b2b64dc6e!2z0L4uINCh0LIuINCV0LvQtdC90Ys!5e0!3m2!1sru!2sua!4v1655885634241!5m2!1sru!2sua" width="100%" height="100%" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+        let map_text = document.createElement('h2');
+        map_text.innerHTML = 'Yes! We are in the middle of nowhere!'
+        map.appendChild(map_text);
+        eightydiv.appendChild(map);
     }
 
-
+    mapping();
 
 
 
